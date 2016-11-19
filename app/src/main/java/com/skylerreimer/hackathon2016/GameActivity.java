1b4af1d094afd73556ad7f985b6baecee36fc9c5
skylerreimer <skylerreimer@gmail.com>
2016-11-19 14:28:50 -0500

    buttons

---
+++ app/src/main/java/com/skylerreimer/hackathon2016/GameActivity.java
@@ -0,0 +1,18 @@
+package com.skylerreimer.hackathon2016;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class GameActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.game);
+    }
+
+}
--- app/src/main/java/com/skylerreimer/hackathon2016/MainActivity.java
+++ app/src/main/java/com/skylerreimer/hackathon2016/MainActivity.java
@@ -1,6 +1,7 @@
 package com.skylerreimer.hackathon2016;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -79,4 +80,11 @@ public class MainActivity extends AppCompatActivity {
             button.setText(R.string.soundOn);
         }
     }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+
+        startActivity(intent);
+    }
+
 }
