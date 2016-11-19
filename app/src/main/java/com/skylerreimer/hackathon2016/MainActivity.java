004fda642d94a71de2512fd813c413b632295499
skylerreimer <skylerreimer@gmail.com>
2016-11-19 13:03:17 -0500

    buttons

--- app/src/main/java/com/skylerreimer/hackathon2016/MainActivity.java
+++ app/src/main/java/com/skylerreimer/hackathon2016/MainActivity.java
@@ -1,6 +1,7 @@
 package com.skylerreimer.hackathon2016;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.AudioFile1);
+        mp.start();
         setContentView(R.layout.activity_main);
     }
 
