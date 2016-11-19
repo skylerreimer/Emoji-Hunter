2aa37cdd1dc2f99e80c26931fb76e38c64f846f1
krishnadg <krishnadg@gmail.com>
2016-11-19 12:50:52 -0500

    Added emoji collection and created emoji generator class

---
+++ app/src/main/java/com/skylerreimer/hackathon2016/EmojiGenerator.java
@@ -0,0 +1,22 @@
+package com.skylerreimer.hackathon2016;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.graphics.Bitmap;
+
+
+public class EmojiGenerator {
+
+
+
+
+
+
+
+}
