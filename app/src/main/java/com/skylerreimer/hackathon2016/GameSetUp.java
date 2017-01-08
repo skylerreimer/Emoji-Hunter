f5ceea0222a6a3362385fd518e45f31f6406db56
skyler <skylerreimer@gmail.com>
2017-01-08 15:23:03 -0500

    merged with andrew

--- app/src/main/java/com/skylerreimer/hackathon2016/GameSetUp.java
+++ app/src/main/java/com/skylerreimer/hackathon2016/GameSetUp.java
@@ -5,40 +5,46 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
-public class GameActivity extends AppCompatActivity {
-    MediaPlayer mp = null;
-    DrawingBackground d;
-
+public class GameSetUp extends AppCompatActivity {
+    //media player variable intialized to null
+    private MediaPlayer mp = null;
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //super class to start
         super.onCreate(savedInstanceState);
 
-
+        //setting up default window full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        //drawing background and setting user view to this background
 
-        d = new DrawingBackground(this);
-        setContentView(d);
+        game = new Game(this);
+
+        this.setContentView(game);
     }
-
-
 
     @Override
     protected void onStart() {
         super.onStart();  // Always call the superclass method first
 
-        mp = MediaPlayer.create(getApplicationContext(), R.raw.audiofile);
-        mp.start();
+        //get and start music for game
+        //mp = MediaPlayer.create(getApplicationContext(), R.raw.audiofile);
+        //TODO
+        // mp.start();
+
+        game.startGame();
     }
 
     @Override
     protected void onStop() {
         super.onStop();  // Always call the superclass method first
-        mp.stop();
+
+        //stop game music
+        //TODO
+        // mp.stop();
     }
 }
